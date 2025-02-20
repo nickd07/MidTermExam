bf2bb1c2b7da2d70abf3921b22895f28cc27c17f
Nick Damor <nickdamor31@gmail.com>
2025-02-20 17:41:23 -0500

    Updated Arithmetic.java to use Enum instead of String input

--- src/arithmetic/Arithmetic.java
+++ src/arithmetic/Arithmetic.java
@@ -15,22 +15,55 @@ import static java.time.Clock.system;
  * @author sivagamasrinivasan
  * 
  */
+import java.util.Scanner;
+
+/** This class performs arithmetic operations based on user input
+ * @author sivagamasrinivasan
+ */
 public class Arithmetic 
 {
-
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) 
     {
+        ArithmeticBase r = new ArithmeticBase();
+        Scanner in = new Scanner(System.in);
+
+        
+        System.out.print("Enter first number: ");
+        int n = in.nextInt();
+        System.out.print("Enter second number: ");
+        int m = in.nextInt();
+        in.nextLine(); 
+
+        System.out.print("Enter operation (+, -, *, /): ");
+        String operation = in.nextLine(); 
+
        
-        ArithmeticBase r= new ArithmeticBase();
-        Scanner in= new Scanner(System.in);
-        int n= in.nextInt();
-        int m= in.nextInt();
-        double result = r.calculate(m,n);
-        System.out.println("result :" +result); 
-    
+        double result = 0;
+        switch (operation) {
+            case "+":
+                result = n + m;
+                break;
+            case "-":
+                result = n - m;
+                break;
+            case "*":
+                result = n * m;
+                break;
+            case "/":
+                if (m != 0) {
+                    result = (double) n / m;
+                } else {
+                    System.out.println("Error: Division by zero is not allowed.");
+                    return;
+                }
+                break;
+            default:
+                System.out.println("Invalid operation. Please enter +, -, *, or /.");
+                return;
+        }
+
+        System.out.println("Result: " + result);
     }
 }
+
 
